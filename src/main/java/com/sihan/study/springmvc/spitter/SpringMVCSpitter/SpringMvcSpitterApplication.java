686f6697f4af7a55d733e4b558d1faf5dd230e12
sihanwang <ocpwj@hotmail.com>
2020-04-20 11:08:16 +0800

    init

---
+++ src/main/java/com/sihan/study/springmvc/spitter/SpringMVCSpitter/SpringMvcSpitterApplication.java
@@ -0,0 +1,13 @@
+package com.sihan.study.springmvc.spitter.SpringMVCSpitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcSpitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcSpitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sihan/study/springmvc/spitter/SpringMVCSpitter/SpringMvcSpitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sihan.study.springmvc.spitter.SpringMVCSpitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcSpitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
